804ad4e8ecaa03fa9cca1494427878062b09e83c
abrahamodianjo <odianjoabraham@outlook.com>
2023-08-13 20:51:40 +0100

    configured logout button

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
@@ -10,6 +10,16 @@ class AdminController extends Controller
             return view('admin.index');
         
     }//end of AdminDashboard
+
+    public function AdminLogout(Request $request){
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }//end of destroy
 
     
 }//end of AdminController
