6ba73b3e0d701ee45ec57347e8365fb67213c821
abrahamodianjo <odianjoabraham@outlook.com>
2023-08-16 12:34:26 +0100

    Designed the user profile interface

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,16 +2,53 @@
 
 namespace App\Http\Controllers;
 
+
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-   public function Index(){
-        return view('frontend.index');
+   public function Index()
+   {
+      return view('frontend.index');
+   } //end method
 
-   }//end method
+   public function UserProfile()
+   {
 
 
+      $id = Auth::user()->id;
+      $profileData = User::find($id);
+      return view('frontend.dashboard.edit_profile', compact('profileData'));
+   } //end method
+
+   public function UserStore(Request $request)
+   {
+      $id = Auth::user()->id;
+      $data = User::find($id);
+      $data->name = $request->name;
+      $data->email = $request->email;
+      $data->phone = $request->phone;
+      $data->address = $request->address;
+
+      if ($request->file('photo')) {
+         $file = $request->file('photo');
+         @unlink(public_path('upload/user_images/' . $data->photo));
+         $filename = date('YmdHi') . $file->getClientOriginalName();
+         $file->move(public_path('upload/user_images/'), $filename);
+         $data['photo'] = $filename;
+      }
+
+      $data->save();
+
+      $notification = array(
+         'message' => 'Profile Updated Successfully',
+         'alert-type' => 'success',
+      );
+      return redirect()->back()->with($notification);
+   } //end of Admin method
 
 
 
--- resources/views/frontend/main_master.blade.php
+++ resources/views/frontend/main_master.blade.php
@@ -33,7 +33,9 @@
 
         <!-- Favicon -->
         <link rel="icon" type="image/png" href="{{asset('frontend/assets/img/favicon.png')}}">
-
+<!-- toaster Style CSS -->
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" >
+	
         <title>House of Koye</title>
     </head>
     <body>
@@ -92,6 +94,29 @@
         <script src="        {{asset('frontend/assets/js/contact-form-script.js')}}"></script>
         <!-- Custom JS -->
         <script src="        {{asset('frontend/assets/js/custom.js')}}"></script>
-        
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
+<script>
+ @if(Session::has('message'))
+ var type = "{{ Session::get('alert-type','info') }}"
+ switch(type){
+    case 'info':
+    toastr.info(" {{ Session::get('message') }} ");
+    break;
+
+    case 'success':
+    toastr.success(" {{ Session::get('message') }} ");
+    break;
+
+    case 'warning':
+    toastr.warning(" {{ Session::get('message') }} ");
+    break;
+
+    case 'error':
+    toastr.error(" {{ Session::get('message') }} ");
+    break; 
+ }
+ @endif 
+</script>
     </body>
 </html>
