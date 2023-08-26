f54c93c32d91d5832934ed7b9e457f7fd45a4cc5
abrahamodianjo <odianjoabraham@outlook.com>
2023-08-27 00:39:08 +0100

    Edit room setup -4 (for the basic facility functionality)

--- app/Http/Controllers/Backend/RoomController.php
+++ app/Http/Controllers/Backend/RoomController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Facility;
 use Illuminate\Http\Request;
 use App\Models\Room;
 use Intervention\Image\Facades\Image;
@@ -11,8 +12,9 @@ class RoomController extends Controller
 {
     public function EditRoom($id){
 
+        $basic_facility = Facility::where('rooms_id', $id)->get();
         $editData = Room::find($id);
-        return view('backend.allroom.rooms.edit_rooms', compact('editData'));
+        return view('backend.allroom.rooms.edit_rooms', compact('editData', 'basic_facility'));
 
     }//End Method
 }
