0ac95b8b7ea955df3cccae285ed04fe5a8251d01
abrahamodianjo <odianjoabraham@outlook.com>
2023-09-11 01:41:00 +0100

    Check Room availablity setup-2

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -19,14 +19,13 @@
 <div class="banner-form-area">
     <div class="container">
         <div class="banner-form">
-            <form>
+            <form >
                 <div class="row align-items-center">
                     <div class="col-lg-3 col-md-3">
                         <div class="form-group">
                             <label>CHECK IN TIME</label>
                             <div class="input-group">
-                                <input id="datetimepicker" type="text" class="form-control" placeholder="11/02/2020">
-                                <span class="input-group-addon"></span>
+                                <input autocomplete="off" type="text" required class="form-control dt_picker"  name="check_in" placeholder="yyy-mm-dd">  <span class="input-group-addon"></span>
                             </div>
                             <i class='bx bxs-chevron-down'></i>	
                         </div>
@@ -36,7 +35,7 @@
                         <div class="form-group">
                             <label>CHECK OUT TIME</label>
                             <div class="input-group">
-                                <input id="datetimepicker-check" type="text" class="form-control" placeholder="11/02/2020">
+                                <input autocomplete="off" required type="text" class="form-control dt_picker"  name="check_out" placeholder="yyy-mm-dd">
                                 <span class="input-group-addon"></span>
                             </div>
                             <i class='bx bxs-chevron-down'></i>	
@@ -46,7 +45,7 @@
                     <div class="col-lg-2 col-md-2">
                         <div class="form-group">
                             <label>GUESTS</label>
-                            <select class="form-control">
+                            <select name="persion" class="form-control">
                                 <option>01</option>
                                 <option>02</option>
                                 <option>03</option>
@@ -57,10 +56,13 @@
 
                     <div class="col-lg-4 col-md-4">
                         <button type="submit" class="default-btn btn-bg-one border-radius-5">
-                            Check Arability
+                            Check Availability
                         </button>
                     </div>
                 </div>
+
+
+
             </form>
         </div>
     </div>
