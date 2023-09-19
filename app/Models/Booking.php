894c7847ac2b28b84ac837308ab2212430ea30a7
abrahamodianjo <odianjoabraham@outlook.com>
2023-09-19 10:12:38 +0100

    Check Room availablity setup-5

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -9,4 +9,9 @@ class Booking extends Model
 {
     use HasFactory;
     protected $guarded = [];
+
+    public function assign_rooms(){
+        return $this->hasMany(BookingRoomList::class, 'booking_id');
+    } // this is to create a relationship between booking and booking room list 
+    //for us to get the booking id from bopokingroomlist model
 }
