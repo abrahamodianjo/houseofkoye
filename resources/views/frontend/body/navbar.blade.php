a758f87d0efc583068e2f24dbddbe8a4abe2b644
abrahamodianjo <odianjoabraham@outlook.com>
2023-09-19 13:39:23 +0100

    Check Room details-2

--- resources/views/frontend/body/navbar.blade.php
+++ resources/views/frontend/body/navbar.blade.php
@@ -1,7 +1,7 @@
 <div class="navbar-area">
     <!-- Menu For Mobile Device -->
     <div class="mobile-nav">
-        <a href="index.html" class="logo">
+        <a href="{{url('/')}}" class="logo">
             
             <img src="{{asset('frontend/assets/img/logos/logo-1.png')}}" class="logo-one" alt="Logo">
             <img src="{{asset('frontend/assets/img/logos/footer-logo1.png')}}" class="logo-two" alt="Logo">
@@ -12,7 +12,7 @@
     <div class="main-nav">
         <div class="container">
             <nav class="navbar navbar-expand-md navbar-light ">
-                <a class="navbar-brand" href="index.html">
+                <a class="navbar-brand" href="{{url('/')}}">
                     <img src="{{asset('frontend/assets/img/logos/logo-1.png')}}" class="logo-one" alt="Logo">
                     <img src="{{asset('frontend/assets/img/logos/footer-logo1.png')}}" class="logo-two" alt="Logo">
                 </a>
--- resources/views/frontend/room/search_room.blade.php
+++ resources/views/frontend/room/search_room.blade.php
@@ -46,11 +46,11 @@
 
             <div class="col-lg-4 col-md-6">
                 <div class="room-card">
-                <a href="{{route('search_room_details',$item->id.'&check_in='.old(check_in).'&check_out='.old('check_out').'&persion='.old('persion'))}}">{{ $item['type']['name'] }}</a>
-                        <img src="{{asset('upload/roomimg/'.$item->image)}}" alt="Images" style="width:500px; height:450px">
+                    <a href="{{ route('search_room_details',$item->id.'?check_in='.old('check_in').'&check_out='.old('check_out').'&persion='.old('persion'))}}">
+                         <img src="{{asset('upload/roomimg/'.$item->image)}}" alt="Images" style="width:500px; height:450px">
                     </a>
                     <div class="content">
-                        <h6><a href="{{route('search_room_details',$item->id.'&check_in='.old(check_in).'&check_out='.old('check_out').'&persion='.old('persion'))}}">{{ $item['type']['name'] }}</a></h6>
+                        <h3><a href="{{ route('search_room_details',$item->id.'?check_in='.old('check_in').'&check_out='.old('check_out').'&persion='.old('persion'))}}">{{ $item['type']['name'] }}</a></h3>
                         <ul>
                             <li class="text-color">{{($item->price)}}</li>
                             <li class="text-color">Per Night</li>
